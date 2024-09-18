58137f7140d1f427f0066e88c68f94520329dd8e
suman <sumanrajkhanal@gmail.com>
2024-09-18 13:22:47 +0545

    change in the github link

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => (
     <footer className="game-footer">
         <p>
             Tapping Adventure Game| Created by Your SRK |
-            <a href="https://github.com/yourusername/taptastic-mood-booster" target="_blank" rel="noopener noreferrer" className="github-link">
+            <a href="https://github.com/srksuman/Tapping-Adventure" target="_blank" rel="noopener noreferrer" className="github-link">
                 GitHub Repository
             </a>
         </p>
